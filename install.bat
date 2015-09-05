96473a06a865883118fed6b652bb840d44105dc6
lanastasov <lubomir.anastasov@gmail.com>
2015-09-06 01:36:16 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,2 +1,20 @@
+:CheckOS
+IF EXIST "%PROGRAMFILES(X86)%" (GOTO 64BIT) ELSE (GOTO 32BIT)
+
+:64BIT
+echo 64-bit...
+
+choco install redis-64 -y
+
+GOTO END
+
+:32BIT
+echo 32-bit...
+
 choco install sharex -y
 choco install autohotkey.install -y
+choco install redis -y 
+
+GOTO END
+
+:END
