b0fa703b3ce4ebb5a5589754934d82eaffc7ef27
lanastasov <lubomir.anastasov@gmail.com>
2015-09-16 05:07:43 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,7 +2,8 @@ REM https://chocolatey.org/
 
 REM sizer
 REM chrome-zoomed: 1303 x 743
-REM meta-zoomed:   1295 x 758  
+REM meta-zoomed:   1295 x 758
+REM st2-zoomed:    1294 x 756
 
 REM sift - grep replacement
 REM http://sift-tool.org/
