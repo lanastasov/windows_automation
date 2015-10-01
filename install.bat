6d110b6eb9f1a40812dd4b400d9191d4fe2f4ae6
lanastasov <lubomir.anastasov@gmail.com>
2015-10-01 23:27:10 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -5,6 +5,10 @@ REM chrome-zoomed: 1303 x 743
 REM meta-zoomed:   1295 x 758
 REM st2-zoomed:    1294 x 756
 
+REM sizer - resize windows
+REM http://www.brianapps.net/sizer/sizer334.msi
+REM windows_automation/apps/sizer334.msi
+
 REM sift - grep replacement
 REM http://sift-tool.org/
 REM go get github.com/svent/sift
@@ -19,6 +23,7 @@ REM WARNING: The data being saved is truncated to 1024 characters.
 
 REM epub reader 
 REM http://www.epubread.com/app/reader.html
+
 
 :CheckOS
 IF EXIST "%PROGRAMFILES(X86)%" (GOTO 64BIT) ELSE (GOTO 32BIT)
