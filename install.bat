b2e73a9a8fec98775f674e1a0b94f16bed6eef35
lanastasov <lubomir.anastasov@gmail.com>
2016-07-27 19:11:47 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -32,6 +32,7 @@ REM http://www.imgburn.com/index.php?act=download&
 
 REM Hijacker
 REM C:\Users\lanastasov\AppData\Local\Google\Chrome\User Data\Default\Local Storage\http_search.safefinder.com_0.localstorage
+REM remove C:\ProgramData\Airtostrong
 
 :CheckOS
 IF EXIST "%PROGRAMFILES(X86)%" (GOTO 64BIT) ELSE (GOTO 32BIT)
