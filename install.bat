6e369608c81e1c38ab0c436810f37c1f836b03f9
lanastasov <lubomir.anastasov@gmail.com>
2021-06-28 17:08:50 +0300

    resolution

--- install.bat
+++ install.bat
@@ -3,6 +3,8 @@ REM https://chocolatey.org/
 REM Acer - Atheros AR5B125 Wireless Network Adapter
 REM      - Broadcom NetLink (TM) Gigabit Ethernet
 
+REM 1920 x 1080
+
 REM sizer
 REM chrome-zoomed: 1290 x 725 window
 REM meta-zoomed:   1295 x 758
