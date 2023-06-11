15180a62d3b22749530fcb7b254efd60b8092074
Lubomir Anastasov <lubomir.anastasov@gmail.com>
2023-06-11 05:39:34 +0300

    manjaro vscode font

--- install.bat
+++ install.bat
@@ -117,6 +117,12 @@ REM VS Code Theme
 REM File -> Preference -> Color Theme [Ctrl+K Ctrl+T]
 REM Ayu Mirage Plus
 
+REM VS Code Font
+REM Zsh displayed correctly in the terminal
+REM https://dev.to/avantar/how-to-fix-zsh-icons-in-visual-studio-code-terminal-38bb
+REM https://github.com/romkatv/powerlevel10k-media/
+REM under manjaro use => MesloLGS NF
+
 REM remote shutdown
 REM http://mswintips.com/Access%20Is%20Denied.(5)%20On%20Remote%20Shutdown%20Fix.php
 REM https://www.groovypost.com/howto/remote-shutdown-restart-windows-10/
